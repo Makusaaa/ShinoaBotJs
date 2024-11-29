2de6a42191e76e3b4813528db822d39bbfd747a0
Max <maxjapri@gmail.com>
2024-11-29 18:07:05 +0700

    manga feature to makumanga

--- commands/utility/manga.js
+++ commands/utility/manga.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { LoadMangaList } = require('../../services/mangaService')
+const { LoadMangaList, LoadMangaChapters, ChapterNumber, GetMangaReaderLink } = require('../../services/mangaService')
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -9,18 +9,41 @@ module.exports = {
 			option.setName('title')
 				.setDescription('Search for manga')
 				.setAutocomplete(true)
+				.setRequired(true)
+		).addStringOption(option => 
+			option.setName('chapter')
+				.setDescription('Search chapter')
+				.setAutocomplete(true)
+				.setRequired(true)
 		),
 	async autocomplete(interaction){
 		const focusedValue = interaction.options.getFocused();
-		const mangaList = await LoadMangaList();
-		const limit = 10;
-		const mangaFiltered = mangaList.filter(test => test.s.toLowerCase().includes(focusedValue.toLowerCase())).slice(0,limit);
+		const focusedOption = interaction.options.getFocused(true);
 
-		await interaction.respond(
-			mangaFiltered.map(manga => ({ name: manga.s, value: manga.i })),
-		);
+		if(focusedOption.name === 'title')
+		{
+			const limit = 10;
+			const mangaList = await LoadMangaList();
+			const mangaFiltered = mangaList.filter(manga => manga.name.toLowerCase().includes(focusedValue.toLowerCase())).slice(0,limit);
+			await interaction.respond(mangaFiltered);
+		}
+		if(focusedOption.name === 'chapter')
+		{
+			const mangaId = interaction.options.get('title').value;
+			const limit = 5;
+			const chapterList = await LoadMangaChapters(mangaId);
+			const chapterFiltered = chapterList.filter(chapter => {
+				if(!focusedValue || focusedValue == '' || focusedValue.length == 0) return chapter
+				if(ChapterNumber(chapter.value) == focusedValue)
+					return chapter;
+			}).slice(0,limit)
+			await interaction.respond(chapterFiltered);
+		}
 	},
 	async execute(interaction) {
-		console.log("testing lagi command handling");
+		const mangaId = interaction.options.get('title').value;
+		const chapter = interaction.options.get('chapter').value;
+		const link = `https://makumanga.vercel.app/${mangaId}/${chapter}`;
+		await interaction.reply({content: `<${link}>`, ephemeral: true });
 	},
 };
--- services/mangaService.js
+++ services/mangaService.js
@@ -1,10 +1,12 @@
 const fs = require('fs').promises;
 const axios = require('axios');
 
+const baseURL = 'https://www.manga4life.com'
+
 module.exports = { 
     LoadMangaList: async () => {
         const filePath = "./data/mangaList.json";
-        const mangaURL = "https://manga4life.com/_search.php";
+        const mangaWebURL = baseURL+"/_search.php";
         let dataReturn;
 
         await fs.readFile(filePath)
@@ -12,9 +14,42 @@ module.exports = {
             dataReturn = JSON.parse(data);
         })
         .catch(async ()=>{
-            dataReturn = (await axios.get(mangaURL)).data;
-            await fs.writeFile(filePath,JSON.stringify(dataReturn))
+            dataReturn = (await axios.get(mangaWebURL)).data;
+            await fs.writeFile(filePath,JSON.stringify(dataReturn));
         });
-        return dataReturn;
+        return dataReturn.map(manga => ({
+            name: manga.s,
+            value: manga.i
+        }));
     },
+    LoadMangaChapters: async (mangaId) => {
+        const mangaURL = `${baseURL}/manga/${mangaId}`;
+        const chapters = GetChapters((await axios.get(mangaURL)).data)
+        return chapters.map(chapter => ({
+            name: chapter.Type+' '+ChapterNumber(chapter.Chapter),
+            value: chapter.Chapter
+        }))
+    },
+    ChapterNumber: (chapter) => {
+        const num = parseInt(chapter.slice(1,-1))
+        let coma = ""
+        if(chapter[chapter.length-1] != "0")
+            coma = "."+chapter[chapter.length-1]
+        return num + coma
+    }
+}
+
+const GetChapters = (html) => {
+    const keyword = "vm.Chapters = ";
+    const start = html.slice(html.indexOf(keyword) + keyword.length)
+    const data = start.slice(0,start.indexOf("];")+1)
+    return JSON.parse(data)
+}
+
+const ChapterNumber = (chapter) => {
+    const num = parseInt(chapter.slice(1,-1))
+    let coma = ""
+    if(chapter[chapter.length-1] != "0")
+        coma = "."+chapter[chapter.length-1]
+    return num + coma
 }
